e625329f0d7f33e595f8e5e3b9d8549139be6da6
Raphael Lima <phaelslima@gmail.com>
2026-08-14 22:01:24 -0300

    feat: add Logo component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
+import { Logo } from '../logo';
 
 export const Footer = () => {
   return (
     <footer className="border-t border-white/10">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between md:flex-row gap-8 py-8">
-          <Link href="/">Logo</Link>
+          <Logo />
 
           <nav className="flex flex-col md:flex-row items-center gap-4 text-sm text-muted-foreground">
             <Link href="/termos-de-uso" className="hover:text-primary">
