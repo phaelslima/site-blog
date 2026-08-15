b57276eb4ba4e98473bf43fccaf8bd2fb4fdd375
Raphael Lima <phaelslima@gmail.com>
2026-08-14 21:52:58 -0300

    feat: add Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Header } from "@/components/header";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -16,7 +15,6 @@ export default function Home() {
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <Header />
     </div>
   );
 }
